6db1b52708b05a3e15a253beec13893823bf24ed
AlessandraRogatto <asvieira90@gmail.com>
2022-08-08 17:41:28 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
@@ -9,6 +11,8 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro2 = new Livro( "As Crônicas de gelo e fogo",3500);
+        System.out.println(livro2);
 
 /*        int a = 5;
         int b = 10;
@@ -17,5 +21,52 @@ public class PrimeiroPrograma {
 
 
 
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numpg;
+
+    public Livro(String nome, Integer numpg) {
+        this.nome = nome;
+        this.numpg = numpg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpg() {
+        return numpg;
+    }
+
+    public void setNumpg(Integer numpg) {
+        this.numpg = numpg;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpg=" + numpg +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numpg, livro.numpg);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numpg);
     }
 }
